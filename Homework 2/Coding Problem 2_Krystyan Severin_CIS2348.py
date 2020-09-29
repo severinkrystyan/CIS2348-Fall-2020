7b8c1805ca7d1a6cf1e1edcae05d551bbe538b58
Krystyan Severin <krystyanseverin@gmail.com>
2020-09-29 12:56:33 -0400

    Part B Allows file to be inputted and dated read. Correct dates are then outputted with the required format.

--- Homework 2/Coding Problem 2_Krystyan Severin_CIS2348.py
+++ Homework 2/Coding Problem 2_Krystyan Severin_CIS2348.py
@@ -2,15 +2,26 @@
    PSID: 1916594"""
 months = {'January': '1', 'February': '2', 'March': '3', 'April': '4', 'May': '5', 'June': '6', 'July': '7',
           'August': '8', 'September': '9', 'October': '10', 'November': '11', 'December': '12'}
-month = ""
-day = ""
-year = ""
-date = input("Please enter date: ")
-# Checks to see if comma is included and that the year has four digits
-if date[-6] == ',' and len(date[-4:]) == 4:
-    for mon in months:
-        if date.find(mon) != -1:
-            month = months[mon]
-    year = date[-4:]
-    day = date[-8:-6].replace(" ", "")  # Removes wipe space in case of single digit in day
-    print(f'{month}/{day}/{year}')
+
+with open("inputDates.txt", "r") as f:
+    file = f.readlines()
+    print("--")
+    for line in file:
+        date = line.replace("\n", "")
+        # Resets variables for each iteration
+        month = ""
+        year = ""
+        day = ""
+        # Checks length first to ensure general format//Short hand dates do not exceed 8 characters
+        if len(date[::-1]) > 10:
+            # Checks to see if comma is included and that the year has four digits
+            if date[-6] == ',' and len(date[-4:]) == 4:
+                for mon in months:
+                    if date.find(mon) != -1:
+                        month = months[mon]
+                year = date[-4:]
+                day = date[-8:-6].replace(" ", "")  # Removes white space in case of single digit in day
+                if month:  # Makes sure the month variable is not empty
+                    print(f'{month}/{day}/{year}')
+    f.close()
+print("--")
