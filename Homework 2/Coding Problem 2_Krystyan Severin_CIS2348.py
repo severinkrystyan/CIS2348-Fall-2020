f3c8d4f8c2099bf405c67b6ce5d0044eb0eace81
Krystyan Severin <krystyanseverin@gmail.com>
2020-09-30 19:21:07 -0400

    PART B - Retrieves data from text file and prints date in appropriate format if correct

--- Homework 2/Coding Problem 2_Krystyan Severin_CIS2348.py
+++ Homework 2/Coding Problem 2_Krystyan Severin_CIS2348.py
@@ -3,25 +3,27 @@
 months = {'January': '1', 'February': '2', 'March': '3', 'April': '4', 'May': '5', 'June': '6', 'July': '7',
           'August': '8', 'September': '9', 'October': '10', 'November': '11', 'December': '12'}
 
-date_list = []
-print('Enter "-1" to submit all dates')
-date = input("Please enter date: ")
-while date != '-1':
-    # Resets Variables for each iteration
-    month = ""
-    day = ""
-    year = ""
-    # Checks to see if comma is included and that the year has four digits
-    if date[-6] == ',' and len(date[-4:]) == 4:
-        for mon in months:
-            if date.find(mon) != -1:
-                month = months[mon]
-        year = date[-4:]
-        day = date[-8:-6].replace(" ", "")  # Removes wipe space in case of single digit in day
-        if month:  # Makes sure the month variable is not empty
-            date_list.append((month + "/" + day + "/" + year))
-    date = input("Please enter date: ")
-print("\n--")
-for item in date_list:
-    print(item)
-print("--")
+with open("inputDates.txt", "r") as f:
+    file = f.readlines()
+    print('--')
+    for line in file:
+        date = line.replace("\n", "")
+        # Resets variables for each iteration
+        month = ""
+        year = ""
+        day = ""
+        # Checks length first to ensure general format//Short hand dates do not exceed 8 characters
+        if len(date[::-1]) > 10:
+            # Checks to see if comma is included and that the year has four digits
+            if date[-6] == ',' and len(date[-4:]) == 4:
+                for mon in months:
+                    if date.find(mon) != -1:
+                        month = months[mon]
+                year = date[-4:]
+                day = date[-8:-6].replace(" ", "")  # Removes white space in case of single digit in day
+                if month:  # Makes sure the month variable is not empty
+                    print(f'{month}/{day}/{year}')
+        elif date == "-1":  # Stops iterations if -1 is reached in file
+            print('--')
+            f.close()
+            break
